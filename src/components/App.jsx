e754c7d4709942796ff9b89a62e4813e9bffbcd5
Henrique William <henriquewilliamstos@gmail.com>
2022-10-16 15:26:39 -0300

    more complex style and new content

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,37 @@
-import React, { useState } from 'react'
-import Search from './Search'
+import React, {useState} from 'react'
 import Box from './Box'
+import Search from './Search'
+import { Typography } from '@mui/material'
 
 
 function App() {
+
+  const [city, setCity] = useState('')
+
+  const [temp, setTemp] = useState("")
+
+  const [desc, setDesc] = useState("")
+
+  const [image, setImage] = useState("")
+
+  const [cityName, setCityName] = useState("")
+
+  function getCity(event) {
+      setCity(event.target.value)
+  }
+  
+  function getData(){
+
+      const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=b5ad28dedf1989aba8f4eb9f41591455&units=metric&lang=pt_br'
+
+      fetch(url).then( (response) => response.json()).then( (data) => {
+          setTemp(Math.round(data.main.temp) + '°C')
+          setDesc(data.weather[0].description)
+          setImage('http://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png')
+          setCityName(data.name)
+
+          document.querySelector('.icon').classList.remove('visually-hidden')
+      })}
 
     const [dimensions, setDimensions] = React.useState({ 
             height: window.innerHeight,
@@ -18,11 +46,26 @@ function App() {
               })}
               window.addEventListener('resize', handleResize)       
             })
-    
+
   return (
-    <div className='main' style={{width: dimensions.width, height: dimensions.height}}>
-        <h1 className='text-center'>Weather Checker</h1>
-        <Box />
+
+    <div className='main' style={{width: dimensions.width, height: dimensions.height}}>    
+        <header>
+          <Typography className='' variant="h5" component="h2">Weather Checker</Typography>
+        </header>
+        <div className='container'>
+          <div className='text-center'>
+            <Search change={getCity} click={getData} value={city} />
+          </div>
+          <Box cityName={cityName} image={image} temp={temp} desc={desc} />
+          <div className='row visually-hidden'>
+            <div className='col col-12'>
+              <div className='section2'></div>
+            </div>
+          </div>
+
+          </div>
+      
     </div>
     
 
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,44 +1,46 @@
 import React from 'react'
-import Search from './Search'
-import { useState } from 'react'
+import { Typography } from '@mui/material'
 
-export default function Box() {
 
-    const [city, setCity] = useState('')
+export default function Box(prop) {
 
-    const [temp, setTemp] = useState("")
-
-    const [desc, setDesc] = useState("")
-
-    const [image, setImage] = useState("")
-
-    function getCity(event) {
-        setCity(event.target.value)
+    const now = new Date()
+    const options = { weekday: 'long'};
+    const day = new Intl.DateTimeFormat('pt-BR', options).format(now)
+    const dayUp = day.charAt(0).toUpperCase() + day.slice(1)
+    var minutes = now.getMinutes()
+    if( minutes < 10 ) {
+        minutes = '0' + minutes   
     }
-    
-    function getData(){
+    const currentHour = dayUp + ' ' + now.getHours() + ':' + minutes
 
-        const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=b5ad28dedf1989aba8f4eb9f41591455&units=metric&lang=pt_br'
-
-        fetch(url).then( response => response.json()).then( data => {
-            setTemp(data.main.temp + '°C')
-            setDesc(data.weather[0].description)
-            setImage('http://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png')
-        })}
-
-        
   return (
     <div className='box'>
-        <div className='container'>
+    <div className='container'>
+    <div className='row text-center hour' >
+        <Typography className='hourText' variant="h5" component="h2">{currentHour}</Typography>
+    </div>
         <div className='row text-center'>
-            <Search change={getCity} click={getData} value={city} />
-            <p className='temp'>{temp}</p>
-            <p className='desc'>{desc}</p>
-            <div className='img'>
-            <img src={image} />
+            <div className='cityName'>
+                <Typography variant='h4'>{prop.cityName}</Typography>
+            </div>
+            <div className='col-12 temp'>
+            <Typography variant='h2' sx={{fontWeight: 'bold'}}>{prop.temp}</Typography>
+            </div>
+            <div className='col col-12'>
+                <p className='desc'>{prop.desc}</p>
+            </div>
+            <div className='col col-12'>
+                <div className='img'>
+                    <img className='icon visually-hidden' alt='icon' border='0' src={prop.image} />
+                </div>
             </div>
         </div>
+            
+            
+
         </div>
-    </div>
+        
+        </div>
   )
 }
