fc6ef8bb8ea1729f3fe04daebbef3bacfa267da2
Henrique William <henriquewilliamstos@gmail.com>
2022-10-17 15:34:37 -0300

    improving ui even more, added reload and day/night background feature

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Typography } from '@mui/material'
+import ReplayIcon from '@mui/icons-material/Replay';
 
 
 
@@ -13,13 +14,29 @@ export default function Box(prop) {
     if( minutes < 10 ) {
         minutes = '0' + minutes   
     }
-    const currentHour = dayUp + ' ' + now.getHours() + ':' + minutes
+    const currentHour = dayUp + ', ' + now.getHours() + ':' + minutes
+
+    var time = Boolean
+
+    useEffect( () => {
+   
+        if (now.getHours() >= 6 && now.getHours() <= 18  ) {
+            time = true
+        } else {
+            time = false
+        }
+
+        console.log(time ? 'day' : 'night')
+        }
+    )
 
   return (
-    <div className='box'>
+    <div style={time ? {backgroundImage: 'url("https://images.unsplash.com/photo-1514477917009-389c76a86b68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1067&q=80")'} : {backgroundImage: 'url("https://images.unsplash.com/flagged/photo-1553475873-55d8c03e9f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80")'}} className='box'>
     <div className='container'>
     <div className='row text-center hour' >
         <Typography className='hourText' variant="h5" component="h2">{currentHour}</Typography>
+        <button className='reload' onClick={prop.refresh}><ReplayIcon /> </button>
+        
     </div>
         <div className='row text-center'>
             <div className='cityName'>
@@ -33,7 +50,7 @@ export default function Box(prop) {
             </div>
             <div className='col col-12'>
                 <div className='img'>
-                    <img className='icon visually-hidden' alt='icon' border='0' src={prop.image} />
+                    <img className='icon visually-hidden' alt='icon' border='0' src={prop.icon} />
                 </div>
             </div>
         </div>
