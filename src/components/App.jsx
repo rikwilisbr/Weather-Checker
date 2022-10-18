83be4ec7ec2dada05c1c96ca938fc2967561040a
Henrique William <henriquewilliamstos@gmail.com>
2022-10-18 13:24:39 -0300

    modern style applied, some new features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useRef, useLayoutEffect} from 'react'
 import Box from './Box'
 import Search from './Search'
 import { Typography } from '@mui/material'
@@ -8,7 +8,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function App() {
 
-  const [city, setCity] = useState('New York')
+  const [city, setCity] = useState('')
 
   const [temp, setTemp] = useState("")
 
@@ -18,7 +18,12 @@ function App() {
 
   const [cityName, setCityName] = useState("")
 
+  const [feelsLike, setFeelsLike] = useState("")
+
   const [errorMessage, setErrorMessage] = useState(Boolean)
+
+
+
 
   function getCity(event) {
       setCity(event.target.value)
@@ -27,11 +32,12 @@ function App() {
   function getData(){
       const url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+ process.env.REACT_APP_KEY +'&units=metric&lang=pt_br'
       fetch(url).then( (response) => response.json()).then( (data) => {
-        if (data.cod == 200){
+        if (data.cod === 200){
           setTemp(Math.round(data.main.temp) + '°C')
           setDesc(data.weather[0].description)
           setIcon('http://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png')
           setCityName(data.name)
+          setFeelsLike('Feels Like ' + Math.round(data.main.feels_like)+'°C')
 
           document.querySelector('.icon').classList.remove('visually-hidden')
           setErrorMessage(false)
@@ -39,10 +45,52 @@ function App() {
           setErrorMessage(true)
           document.querySelector('.icon').classList.add('visually-hidden')
         }
+        
 
       })}
 
-  window.onload = getData()
+      
+      function getCurrentData(pos){
+
+        const lat = String(pos.coords.latitude)
+        const lon = String(pos.coords.longitude)
+
+        const url = 'https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&units=metric&lang=pt_br&appid='+process.env.REACT_APP_KEY
+        fetch(url).then( (response) => response.json()).then( (data) => {
+          if (data.cod === 200){
+            setTemp(Math.round(data.main.temp) + '°C')
+            setDesc(data.weather[0].description)
+            setIcon('http://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png')
+            setCityName(data.name)
+            setFeelsLike('Feels Like ' + Math.round(data.main.feels_like)+'°C')
+  
+            document.querySelector('.icon').classList.remove('visually-hidden')
+            setErrorMessage(false)
+          } else {
+            setErrorMessage(true)
+            document.querySelector('.icon').classList.add('visually-hidden')
+          }
+          
+  
+        })}
+
+      function initCoords() {
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getCurrentData, console.log);
+          } else {
+            alert("Your browser does not support Geolocation!");
+          }
+        }
+    
+    const isFirsRender = useRef(true)
+
+    useLayoutEffect(()=> {
+      if (isFirsRender.current) {
+        initCoords()
+        isFirsRender.current = false
+        return;
+      }
+    })
 
     const [dimensions, setDimensions] = React.useState({ 
             height: window.innerHeight,
@@ -66,17 +114,17 @@ function App() {
               <Search change={getCity} click={getData} value={city} />
             </div>
             <div>
-              <a href='https://github.com/rikwilisbr/Weather-Checker' target='_blank' className='social-icon'>
+              <a href='https://github.com/rikwilisbr/Weather-Checker' target='_blank' rel='noreferrer' className='social-icon'>
                 <GitHubIcon />
               </a>
-              <a href="https://www.linkedin.com/in/henrique-william-495991219/" target="_blank" className='social-icon2'>
+              <a href="https://www.linkedin.com/in/henrique-william-495991219/" target="_blank" rel='noreferrer' className='social-icon2'>
                 <LinkedInIcon />
               </a>
             </div>
         </header>
-        <div className='main' style={{width: dimensions.width, height: dimensions.height}}>    
-          <div className='container'>
-            <Box refresh={() => document.location.reload()} cityName={errorMessage ? 'Not found :(' : cityName} icon={errorMessage ? '' : icon} temp={errorMessage ? '' : temp} desc={errorMessage ? '' : desc} />
+        <div>    
+          <div className='container-fluid p-0 main' style={{height: dimensions.height}}>
+            <Box feelsLike={errorMessage? '' : feelsLike} refresh={() => document.location.reload()} cityName={errorMessage ? 'Not found :(' : cityName} icon={errorMessage ? '' : icon} temp={errorMessage ? '' : temp} desc={errorMessage ? '' : desc} />
             <div className=''>
               <div className='col col-12'>
                 <div className='section2'></div>
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Typography } from '@mui/material'
 import ReplayIcon from '@mui/icons-material/Replay';
 
@@ -16,14 +16,14 @@ export default function Box(prop) {
     }
     const currentHour = dayUp + ', ' + now.getHours() + ':' + minutes
 
-    var time = Boolean
+    const time = useRef(Boolean)
 
     useEffect( () => {
    
         if (now.getHours() >= 6 && now.getHours() <= 18  ) {
-            time = true
+            time.current = true
         } else {
-            time = false
+            time.current = false
         }
 
         console.log(time ? 'day' : 'night')
@@ -32,11 +32,10 @@ export default function Box(prop) {
 
   return (
     <div style={time ? {backgroundImage: 'url("https://images.unsplash.com/photo-1514477917009-389c76a86b68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1067&q=80")'} : {backgroundImage: 'url("https://images.unsplash.com/flagged/photo-1553475873-55d8c03e9f5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80")'}} className='box'>
-    <div className='container'>
-    <div className='row text-center hour' >
+    <div className='container-fluid p-0 m-0'>
+    <div className='row text-center hour m-0' >
         <Typography className='hourText' variant="h5" component="h2">{currentHour}</Typography>
         <button className='reload' onClick={prop.refresh}><ReplayIcon /> </button>
-        
     </div>
         <div className='row text-center'>
             <div className='cityName'>
@@ -45,12 +44,17 @@ export default function Box(prop) {
             <div className='col-12 temp'>
             <Typography variant='h2' sx={{fontWeight: 'bold'}}>{prop.temp}</Typography>
             </div>
-            <div className='col col-12'>
-                <p className='desc'>{prop.desc}</p>
+            <div className='feelslike'>
+                <Typography className='text-center' variant='h7' sx={{fontWeight: 'normal'}}>{prop.feelsLike}</Typography>
             </div>
-            <div className='col col-12'>
-                <div className='img'>
-                    <img className='icon visually-hidden' alt='icon' border='0' src={prop.icon} />
+            <div className='desc-area'>
+                <div className='desc-child'>
+                    <Typography  variant='h6' sx={{fontWeight: 'bold'}} className='desc'>{prop.desc}</Typography>
+                </div>
+                <div className='desc-child'>
+                    <div className='img'>
+                        <img className='icon visually-hidden' alt='icon' border='0' src={prop.icon} />
+                    </div>
                 </div>
             </div>
         </div>
